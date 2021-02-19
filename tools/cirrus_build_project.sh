2c782203341ac085ec882afdb3f4af554788ad70
Jeremy Rand <jeremyrand@airmail.cc>
2021-02-19 00:43:32 +0000

    Cirrus: Don't fail a task if it saved a checkpoint

--- tools/cirrus_build_project.sh
+++ tools/cirrus_build_project.sh
@@ -64,7 +64,8 @@ fi
 
 if [[ "$SHOULD_BUILD" -eq 1 ]]; then
     echo "Building project..."
-    ./rbm/rbm build "$PROJECT" --target "$CHANNEL" --target ncdns-"$OS"-"$ARCH"
+    # If rbm fails, we consider it a success as long as it saved a checkpoint.
+    ./rbm/rbm build "$PROJECT" --target "$CHANNEL" --target ncdns-"$OS"-"$ARCH" || ls ./tmp/interrupted_dirs/*
 else
     #echo "This is a cache-only task, skipping build."
     echo "Skipping build."

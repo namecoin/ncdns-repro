3f8912883ef5054a959c5e4331b0245fe2323ec6
Jeremy Rand <jeremyrand@airmail.cc>
2021-02-19 01:00:32 +0000

    Cirrus: Add 2nd iteration for goeasyconfig

--- tools/cirrus_gen_yml.sh
+++ tools/cirrus_gen_yml.sh
@@ -34,8 +34,10 @@ print_os_arch () {
     fi
 
     # TODO fine-tune this list
-    for PROJECT in goeasyconfig ncdns ncp11 ncprop279 plain-binaries release; do
-        echo "${CHANNEL}_${OS}_${ARCH}_${PROJECT}_docker_builder:
+    for PROJECT in goeasyconfig.1 goeasyconfig.2 ncdns.1 ncp11.1 ncprop279.1 plain-binaries.1 release.1; do
+        PROJECT_BASE=$(echo $PROJECT | cut -d . -f 1)
+        PROJECT_ITER=$(echo $PROJECT | cut -d . -f 2)
+        echo "${CHANNEL}_${OS}_${ARCH}_${PROJECT_BASE}_${PROJECT_ITER}_docker_builder:
   timeout_in: 120m
   out_${CHANNEL}_${OS}_${ARCH}_cache:
     folder: out
@@ -62,18 +64,19 @@ print_os_arch () {
     - sleep 110m
     - ./tools/container-interrupt.sh
   build_script:
-    - \"./tools/cirrus_build_project.sh ${PROJECT} ${CHANNEL} ${OS} ${ARCH} 1\""
+    - \"./tools/cirrus_build_project.sh ${PROJECT_BASE} ${CHANNEL} ${OS} ${ARCH} 1\""
 
         # Depend on previous project
-        if [[ "$PROJECT" == "goeasyconfig" ]]; then
+        if [[ "$PROJECT" == "goeasyconfig.1" ]]; then
             echo "  depends_on:
     - \"${CHANNEL}_${OS}_${ARCH}_download\""
         else
             echo "  depends_on:
-    - \"${CHANNEL}_${OS}_${ARCH}_${PREV_PROJECT}\""
+    - \"${CHANNEL}_${OS}_${ARCH}_${PREV_PROJECT_BASE}_${PREV_PROJECT_ITER}\""
         fi
 
-        local PREV_PROJECT="$PROJECT"
+        local PREV_PROJECT_BASE="$PROJECT_BASE"
+        local PREV_PROJECT_ITER="$PROJECT_ITER"
         echo ""
     done
 }

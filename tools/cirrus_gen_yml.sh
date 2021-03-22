c8501ab9c6508630741bc60952c38f3342f9e032
Jeremy Rand <jeremyrand@airmail.cc>
2021-03-22 14:45:40 +0000

    Cirrus: Set $HOME in signing VM

--- tools/cirrus_gen_yml.sh
+++ tools/cirrus_gen_yml.sh
@@ -121,7 +121,8 @@ print_os_arch () {
             echo '  only_if: $CIRRUS_REPO_OWNER == "namecoin"'
             echo "  env:
     SIGN_BUILD: 1
-    SIGN_KEY: ENCRYPTED[33d4594d76774e6447dfd9fabee90f6214b34e209fa1c1c2ce93ed1a40447a235b013b78afe85db52d5561651a821be1]"
+    SIGN_KEY: ENCRYPTED[33d4594d76774e6447dfd9fabee90f6214b34e209fa1c1c2ce93ed1a40447a235b013b78afe85db52d5561651a821be1]
+    HOME: /root"
         else
             echo "  env:
     SIGN_BUILD: 0"

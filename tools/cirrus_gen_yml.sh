7bd9895c17635b7f332ecb770ed917a7ebe02225
Jeremy Rand <jeremyrand@airmail.cc>
2021-04-06 09:34:12 +0000

    Cirrus: Use YAML matrix for OS/ARCH

--- tools/cirrus_gen_yml.sh
+++ tools/cirrus_gen_yml.sh
@@ -4,115 +4,119 @@ set -euxo pipefail
 shopt -s nullglob globstar
 
 print_os_arch () {
-    local OS="$1"
-    local ARCH="$2"
-
     # Pre-download tarballs and Git repos
-    echo "${CHANNEL}_${OS}_${ARCH}_download_docker_builder:
+    echo "${CHANNEL}_download_docker_builder:
+  name: \"${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH download\"
   timeout_in: 120m
-  out_${CHANNEL}_${OS}_${ARCH}_cache:
+  out_${CHANNEL}_cache:
     folder: out
     fingerprint_script:
-      - \"echo out_${CHANNEL}_${OS}_${ARCH}\"
+      - \"echo out ${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH\"
     reupload_on_changes: true
     populate_script:
       - \"mkdir -p out\"
-  out1_${CHANNEL}_${OS}_${ARCH}_cache:
+  out1_${CHANNEL}_cache:
     folder: out_cache1
     fingerprint_script:
-      - \"echo out1_${CHANNEL}_${OS}_${ARCH}\"
+      - \"echo out1 ${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH\"
     reupload_on_changes: true
     populate_script:
       - \"mkdir -p out_cache1\"
-  git_${CHANNEL}_${OS}_${ARCH}_cache:
+  git_${CHANNEL}_cache:
     folder: git_clones
     fingerprint_script:
-      - \"echo git_${CHANNEL}_${OS}_${ARCH}\"
+      - \"echo git ${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH\"
     reupload_on_changes: true
     populate_script:
       - \"mkdir -p git_clones\"
-  interrupted_aa_${CHANNEL}_${OS}_${ARCH}_cache:
+  interrupted_aa_${CHANNEL}_cache:
     folder: tmp/interrupted_dirs.tar.gz.partaa.folder
     fingerprint_script:
-      - \"echo interrupted_aa_${CHANNEL}_${OS}_${ARCH}\"
+      - \"echo interrupted_aa ${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH\"
     reupload_on_changes: true
-  interrupted_ab_${CHANNEL}_${OS}_${ARCH}_cache:
+  interrupted_ab_${CHANNEL}_cache:
     folder: tmp/interrupted_dirs.tar.gz.partab.folder
     fingerprint_script:
-      - \"echo interrupted_ab_${CHANNEL}_${OS}_${ARCH}\"
+      - \"echo interrupted_ab ${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH\"
     reupload_on_changes: true
-  interrupted_ac_${CHANNEL}_${OS}_${ARCH}_cache:
+  interrupted_ac_${CHANNEL}_cache:
     folder: tmp/interrupted_dirs.tar.gz.partac.folder
     fingerprint_script:
-      - \"echo interrupted_ac_${CHANNEL}_${OS}_${ARCH}\"
+      - \"echo interrupted_ac ${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH\"
     reupload_on_changes: true
   build_script:
-    - \"./tools/cirrus_build_project.sh plain-binaries ${CHANNEL} ${OS} ${ARCH} 0\""
-    echo "  env:
-    CIRRUS_LOG_TIMESTAMP: true"
+    - \"./tools/cirrus_build_project.sh plain-binaries ${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH 0\"
+  env:
+    CIRRUS_LOG_TIMESTAMP: true
+  matrix:
+    - env:
+        CI_RBM_OS: linux
+        CI_RBM_ARCH: x86_64
+    - env:
+        CI_RBM_OS: linux
+        CI_RBM_ARCH: i686
+    - env:
+        CI_RBM_OS: windows
+        CI_RBM_ARCH: x86_64
+    - env:
+        CI_RBM_OS: windows
+        CI_RBM_ARCH: i686
+    - env:
+        CI_RBM_OS: osx
+        CI_RBM_ARCH: x86_64"
     echo ""
 
     # TODO fine-tune this list
     for PROJECT in compiler.1 compiler.2 goeasyconfig.1 ncdns.1 ncp11.1 ncprop279.1 plain-binaries.1 release.nosign release.sign; do
         PROJECT_BASE=$(echo $PROJECT | cut -d . -f 1)
-        if [[ "$PROJECT_BASE" == "compiler" ]]; then
-            if [[ "$OS" == "android" ]]; then
-                PROJECT_BASE=android-toolchain
-            fi
-            if [[ "$OS" == "linux" ]]; then
-                PROJECT_BASE=gcc
-            fi
-            if [[ "$OS" == "windows" ]]; then
-                PROJECT_BASE=mingw-w64
-            fi
-            if [[ "$OS" == "osx" ]]; then
-                PROJECT_BASE=macosx-toolchain
-            fi
-        fi
         PROJECT_ITER=$(echo $PROJECT | cut -d . -f 2)
-        echo "${CHANNEL}_${OS}_${ARCH}_${PROJECT_BASE}_${PROJECT_ITER}_docker_builder:
+        echo "${CHANNEL}_${PROJECT_BASE}_${PROJECT_ITER}_docker_builder:"
+        if [[ "$PROJECT_BASE" == "compiler" ]]; then
+            PROJECT_BASE='$CI_RBM_COMPILER'
+        fi
+        echo "  name: \"${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH ${PROJECT_BASE} ${PROJECT_ITER}\"
   timeout_in: 120m
-  out_${CHANNEL}_${OS}_${ARCH}_cache:
+  out_${CHANNEL}_cache:
     folder: out
     fingerprint_script:
-      - \"echo out_${CHANNEL}_${OS}_${ARCH}\"
+      - \"echo out ${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH\"
     reupload_on_changes: true
     populate_script:
       - \"mkdir -p out\"
-  out1_${CHANNEL}_${OS}_${ARCH}_cache:
+  out1_${CHANNEL}_cache:
     folder: out_cache1
     fingerprint_script:
-      - \"echo out1_${CHANNEL}_${OS}_${ARCH}\"
+      - \"echo out1 ${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH\"
     reupload_on_changes: true
     populate_script:
       - \"mkdir -p out_cache1\"
-  git_${CHANNEL}_${OS}_${ARCH}_cache:
+  git_${CHANNEL}_cache:
     folder: git_clones
     fingerprint_script:
-      - \"echo git_${CHANNEL}_${OS}_${ARCH}\"
+      - \"echo git ${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH\"
     reupload_on_changes: true
     populate_script:
       - \"mkdir -p git_clones\"
-  interrupted_aa_${CHANNEL}_${OS}_${ARCH}_cache:
+  interrupted_aa_${CHANNEL}_cache:
     folder: tmp/interrupted_dirs.tar.gz.partaa.folder
     fingerprint_script:
-      - \"echo interrupted_aa_${CHANNEL}_${OS}_${ARCH}\"
+      - \"echo interrupted_aa ${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH\"
     reupload_on_changes: true
-  interrupted_ab_${CHANNEL}_${OS}_${ARCH}_cache:
+  interrupted_ab_${CHANNEL}_cache:
     folder: tmp/interrupted_dirs.tar.gz.partab.folder
     fingerprint_script:
-      - \"echo interrupted_ab_${CHANNEL}_${OS}_${ARCH}\"
+      - \"echo interrupted_ab ${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH\"
     reupload_on_changes: true
-  interrupted_ac_${CHANNEL}_${OS}_${ARCH}_cache:
+  interrupted_ac_${CHANNEL}_cache:
     folder: tmp/interrupted_dirs.tar.gz.partac.folder
     fingerprint_script:
-      - \"echo interrupted_ac_${CHANNEL}_${OS}_${ARCH}\"
+      - \"echo interrupted_ac ${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH\"
     reupload_on_changes: true
   checkpoint_background_script:
     - sleep 110m
     - ./tools/container-interrupt.sh
   build_script:
-    - \"./tools/cirrus_build_project.sh ${PROJECT_BASE} ${CHANNEL} ${OS} ${ARCH} 1\""
+    - \"./tools/cirrus_build_project.sh ${PROJECT_BASE} ${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH 1\""
 
         if [[ "$PROJECT_BASE" == "release" ]]; then
             echo "  binaries_artifacts:
@@ -130,7 +134,28 @@ print_os_arch () {
     SIGN_BUILD: 0"
         fi
         echo "  env:
-    CIRRUS_LOG_TIMESTAMP: true"
+    CIRRUS_LOG_TIMESTAMP: true
+  matrix:
+    - env:
+        CI_RBM_OS: linux
+        CI_RBM_ARCH: x86_64
+        CI_RBM_COMPILER: gcc
+    - env:
+        CI_RBM_OS: linux
+        CI_RBM_ARCH: i686
+        CI_RBM_COMPILER: gcc
+    - env:
+        CI_RBM_OS: windows
+        CI_RBM_ARCH: x86_64
+        CI_RBM_COMPILER: mingw-w64
+    - env:
+        CI_RBM_OS: windows
+        CI_RBM_ARCH: i686
+        CI_RBM_COMPILER: mingw-w64
+    - env:
+        CI_RBM_OS: osx
+        CI_RBM_ARCH: x86_64
+        CI_RBM_COMPILER: macosx-toolchain"
         if [[ "$PROJECT_ITER" == "nosign" ]]; then
             echo '  only_if: $CIRRUS_REPO_OWNER != "namecoin"'
         fi
@@ -138,10 +163,10 @@ print_os_arch () {
         # Depend on previous project
         if [[ "$PROJECT" == "compiler.1" ]]; then
             echo "  depends_on:
-    - \"${CHANNEL}_${OS}_${ARCH}_download\""
+    - \"${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH download\""
         else
             echo "  depends_on:
-    - \"${CHANNEL}_${OS}_${ARCH}_${PREV_PROJECT_BASE}_${PREV_PROJECT_ITER}\""
+    - \"${CHANNEL} \$CI_RBM_OS \$CI_RBM_ARCH ${PREV_PROJECT_BASE} ${PREV_PROJECT_ITER}\""
         fi
 
         if [[ "$PROJECT_ITER" != "nosign" ]]; then
@@ -154,11 +179,7 @@ print_os_arch () {
 
 (
 for CHANNEL in release; do
-    print_os_arch linux x86_64
-    print_os_arch linux i686
-    print_os_arch windows x86_64
-    print_os_arch windows i686
-    print_os_arch osx x86_64
+    print_os_arch
 done
 ) > .cirrus.yml
 
